c8781fae4920eb54ff73c9633b6b42f63d52e57e
Giovanni Buzzi <giower.gb@gmail.com>
2025-03-14 15:43:41 +0100

    Added parsing logic

--- RouteAnalyzer/src/main/kotlin/Main.kt
+++ RouteAnalyzer/src/main/kotlin/Main.kt
@@ -2,7 +2,20 @@ package org.example
 import java.io.File
 import java.io.InputStream
 import java.io.IO.println
+import javax.lang.model.type.NullType
 
 fun main(){
-    println("Hello World")
+    val inputStream = object {}.javaClass.getResourceAsStream("/waypoints.csv")
+    val lines = inputStream?.bufferedReader()?.readLines()
+    if (lines != null) {
+        for (line in lines){
+            val splitted_line = line.split(";")
+            println("Timestamp : ${splitted_line[0]} - Latitude ${splitted_line[1]} - Longitude ${splitted_line[1]}")
+        }
+    }
+    else{
+        println("Error opening file!")
+    }
+
+
 }
